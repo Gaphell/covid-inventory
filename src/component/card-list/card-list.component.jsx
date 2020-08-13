38a5ba6e8d60ec587455d8fc73b9b10d7c3148e7
Tenzin <tenzin606gaphel@gmail.com>
2020-08-13 17:40:45 +0600

    bind card for listing orders

--- src/component/card-list/card-list.component.jsx
+++ src/component/card-list/card-list.component.jsx
@@ -7,7 +7,7 @@ const orders = [
         order_id: 1,
         name: 'Gaphel',
         contact: 77246416,
-        location: 'Babesa',
+        location: 'babesa',
         order_details: [
             {item_name: 'rice', quantity: '25 kg'},
             {item_name: 'oil', quantity: '1 L'},
@@ -15,9 +15,9 @@ const orders = [
     },
     {
         order_id: 2,
-        name: 'Gaphel',
-        contact: 77246416,
-        location: 'Babesa',
+        name: 'Prakash',
+        contact: 12345678,
+        location: 'olakha',
         order_details: [
             {item_name: 'rice', quantity: '25 kg'},
             {item_name: 'oil', quantity: '1 L'},
@@ -25,14 +25,24 @@ const orders = [
     },
     {
         order_id: 3,
-        name: 'Gaphel',
-        contact: 77246416,
-        location: 'Babesa',
+        name: 'Tashi',
+        contact: 87654321,
+        location: 'changzamtog',
         order_details: [
             {item_name: 'rice', quantity: '25 kg'},
             {item_name: 'oil', quantity: '1 L'},
             {item_name: 'salt', quantity: '1 kg'}]
     },
+    {
+        order_id: 4,
+        name: 'Rinzin',
+        contact: 99797687,
+        location: 'Debsi',
+        order_details: [
+            {item_name: 'rice', quantity: '25 kg'},
+            {item_name: 'oil', quantity: '1 L'},
+            {item_name: 'salt', quantity: '1 kg'}]
+    }
 
 ]
 const CardListComponent = (props) => (
