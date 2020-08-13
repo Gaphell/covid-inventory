901e9ce505ff98d4d7138187cf9a56189a0b1ea5
Tenzin <tenzin606gaphel@gmail.com>
2020-08-13 20:25:32 +0600

    add infinite scroller

--- src/component/card-list/card-list.component.jsx
+++ src/component/card-list/card-list.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './card-list.styles.scss';
 import CardItemComponent from "../card-item/card.component";
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const orders = [
     {
@@ -42,17 +43,83 @@ const orders = [
             {item_name: 'rice', quantity: '25 kg'},
             {item_name: 'oil', quantity: '1 L'},
             {item_name: 'salt', quantity: '1 kg'}]
+    },
+    {
+        order_id: 5,
+        name: 'Rinzin',
+        contact: 99797687,
+        location: 'Debsi',
+        order_details: [
+            {item_name: 'rice', quantity: '25 kg'},
+            {item_name: 'oil', quantity: '1 L'},
+            {item_name: 'salt', quantity: '1 kg'}]
+    },
+    {
+        order_id: 6,
+        name: 'Rinzin',
+        contact: 99797687,
+        location: 'Debsi',
+        order_details: [
+            {item_name: 'rice', quantity: '25 kg'},
+            {item_name: 'oil', quantity: '1 L'},
+            {item_name: 'salt', quantity: '1 kg'}]
+    },
+    {
+        order_id: 7,
+        name: 'Rinzin',
+        contact: 99797687,
+        location: 'Debsi',
+        order_details: [
+            {item_name: 'rice', quantity: '25 kg'},
+            {item_name: 'oil', quantity: '1 L'},
+            {item_name: 'salt', quantity: '1 kg'}]
+    },
+    {
+        order_id: 8,
+        name: 'Rinzin',
+        contact: 99797687,
+        location: 'Debsi',
+        order_details: [
+            {item_name: 'rice', quantity: '25 kg'},
+            {item_name: 'oil', quantity: '1 L'},
+            {item_name: 'salt', quantity: '1 kg'}]
+    }
+];
+
+class CardListComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {orders: orders}
     }
 
-]
-const CardListComponent = (props) => (
-    <div className="card-list">
-        {
-            orders.map((order) => (
-                <CardItemComponent key={order.order_id} order={order}></CardItemComponent>
-            ))
-        }
-    </div>
-)
+    fetchMoreData = () => {
+        setTimeout(() => {
+            this.setState({
+                orders: this.state.orders.concat(orders)
+            })
+        }, 1500);
+
+    };
+
+    render() {
+        return (
+            <InfiniteScroll
+                dataLength={this.state.orders.length}
+                next={this.fetchMoreData}
+                hasMore={true}
+                loader={<h4>Loading...</h4>}>
+                <div className="card-list">
+                    {
+                        this.state.orders.map((order, index) => (
+                            <CardItemComponent key={/*order.order_id*/index} order={order}></CardItemComponent>
+                        ))
+                    }
+                </div>
+            </InfiniteScroll>
+        )
+    }
+
+
+}
 
 export default CardListComponent;
