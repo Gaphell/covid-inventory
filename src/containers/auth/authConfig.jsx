ff231ebe58aad0ef8e2903331fd0fd2ac85a8dbe
Kuenzang <kuenzanggyaltshen@selise.ch>
2020-08-14 12:21:01 +0600

    auth forms all

--- src/containers/auth/authConfig.jsx
+++ src/containers/auth/authConfig.jsx
@@ -20,3 +20,104 @@ export const SignInConfig = {
         }
     }
 };
+
+export const DeliverySignupConfig = {
+    controls: {
+        name: {
+            options: {
+                validators: Validators.required
+            },
+            render: FormInput,
+            meta: {label: "Name", type: "text"}
+        },
+        cid: {
+            options: {
+                validators: Validators.required
+            },
+            render: FormInput,
+            meta: {label: "CID No.", type: "text"}
+        },
+        contact: {
+            options: {
+                validators: Validators.required
+            },
+            render: FormInput,
+            meta: {label: "Contact Number", type: "text"}
+        },
+        email: {
+            options: {
+                validators: Validators.email
+            },
+            render: FormInput,
+            meta: {label: "Email", type: "email"}
+        },
+        password: {
+            options: {
+                validators: Validators.required
+            },
+            render: FormInput,
+            meta: {label: "Password", type: "password"}
+        },
+        c_password: {
+            options: {
+                validators: Validators.required
+            },
+            render: FormInput,
+            meta: {label: "Confirm Password", type: "password"}
+        }
+    }
+};
+
+export const ManagerSignUp = {
+    controls: {
+        name: {
+            options: {
+                validators: Validators.required
+            },
+            render: FormInput,
+            meta: {label: "Name", type: "text"}
+        },
+        cid: {
+            options: {
+                validators: Validators.required
+            },
+            render: FormInput,
+            meta: {label: "CID No.", type: "text"}
+        },
+        contact: {
+            options: {
+                validators: Validators.required
+            },
+            render: FormInput,
+            meta: {label: "Contact Number", type: "text"}
+        },
+        email: {
+            options: {
+                validators: Validators.email
+            },
+            render: FormInput,
+            meta: {label: "Email", type: "email"}
+        },
+        password: {
+            options: {
+                validators: Validators.required
+            },
+            render: FormInput,
+            meta: {label: "Password", type: "password"}
+        },
+        c_password: {
+            options: {
+                validators: Validators.required
+            },
+            render: FormInput,
+            meta: {label: "Confirm Password", type: "password"}
+        },
+        secret_key: {
+            options: {
+                validators: Validators.required
+            },
+            render: FormInput,
+            meta: {label: "Token", type: "text"}
+        }
+    }
+};
--- src/shared/form-generator/formInput.js
+++ src/shared/form-generator/formInput.js
@@ -3,9 +3,12 @@ import React from "react";
 
 export const FormInput = ({ handler, touched, hasError, meta }) => (
     <div>
-        <OutlinedInput label={meta.label} type={meta.type} placeholder={`Enter ${meta.label}`} {...handler()}/>
+        <OutlinedInput label={meta.label} type={meta.type} placeholder={`${meta.label}`} {...handler()}/>
         <FormHelperText error={true} children={touched
         && hasError("required")
         && `${meta.label} is required`}/>
+        <FormHelperText error={true} children={touched
+        && hasError("email")
+        && `Invalid Email`}/>
     </div>
 );
