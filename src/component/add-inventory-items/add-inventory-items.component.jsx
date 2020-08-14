532363990337760ede442e0b4b13744b11968967
Tenzin <tenzin606gaphel@gmail.com>
2020-08-14 15:21:19 +0600

    implement form array form adding inventory items

--- src/component/add-inventory-items/add-inventory-items.component.jsx
+++ src/component/add-inventory-items/add-inventory-items.component.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './add-inventory-items.styles.scss';
-import {InventoryItemConfig} from "./add-inventory-items.config";
-import {FormGenerator} from "react-reactive-form";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
 import DeleteIcon from "@material-ui/icons/Delete";
+import {FormBuilder, FieldArray, FieldGroup, FieldControl} from "react-reactive-form";
+import Input from "@material-ui/core/Input";
 
 class AddInventoryItemComponent extends React.Component {
+    keyCount = 0;
+    inventoryItemForm = FormBuilder.array([])
+
     constructor(props) {
         super(props);
         this.state = {};
@@ -15,45 +18,89 @@ class AddInventoryItemComponent extends React.Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-        if (this.inventoryForm.valid) {
-            this.props.history.push('/home');
-        } else {
-            this.inventoryForm.markAsTouched();
-        }
-        console.log("Form values", this.inventoryForm.value);
+        console.log("Form values", this.inventoryItemForm.value);
     };
 
-    handleReset = () => {
-        this.loginForm.reset();
-    };
+    createItem = () => {
+        const control = FormBuilder.group({
+            name: undefined,
+            quantity: undefined,
+            description: undefined
+        });
+        control.meta = {
+            key: this.getKey()
+        };
+        return control;
+    }
 
-    setForm = (form) => {
-        this.inventoryForm = form;
-        this.inventoryForm.meta = {
-            handleReset: this.handleReset
-        }
+    addItem = () => {
+        this.inventoryItemForm.push(this.createItem());
+    }
+
+    removeItem = (index) => {
+        this.inventoryItemForm.removeAt(index);
+    }
+
+    getKey = () => {
+        return this.keyCount++;
     };
 
     render() {
         return (
             <div>
-                <form noValidate onSubmit={this.handleSubmit}>
-                    <FormGenerator onMount={this.setForm} fieldConfig={InventoryItemConfig}/>
-                    <IconButton aria-label="add" color="primary">
-                        <AddCircleIcon/>
-                    </IconButton>
-                    <IconButton aria-label="delete" color="primary">
-                        <DeleteIcon/>
-                    </IconButton>
-                    <Button
-                        className="custom-button"
-                        type="submit"
-                        variant="contained"
-                        color="primary"
-                    >
-                        Submit
-                    </Button>
-                </form>
+                <IconButton aria-label="add" color="primary" onClick={this.addItem}>
+                    <AddCircleIcon/>
+                </IconButton>
+                <FieldArray
+                    control={this.inventoryItemForm}
+                    render={({controls}) => {
+                        return (
+                            controls.map((itemControl, index) => (
+                                <div key={index}>
+                                    <FieldGroup
+                                        control={itemControl}
+                                        render={() => (
+                                            <div>
+                                                <FieldControl
+                                                    name="name"
+                                                    render={({handler}) => (
+                                                        <div>
+                                                            <label>Name:</label>
+                                                            <Input {...handler()}/>
+                                                        </div>
+                                                    )}/>
+                                                <FieldControl
+                                                    name="description"
+                                                    render={({handler}) => (
+                                                        <div>
+                                                            <label>Description:</label>
+                                                            <Input {...handler()}/>
+                                                        </div>
+                                                    )}/>
+                                                <FieldControl
+                                                    name="quantity"
+                                                    render={({handler}) => (
+                                                        <div>
+                                                            <label>Quantity:</label>
+                                                            <Input {...handler()}/>
+                                                        </div>
+                                                    )}/>
+                                            </div>
+                                        )}/>
+                                    <IconButton aria-label="delete" color="primary"
+                                                onClick={() => this.removeItem(index)}>
+                                        <DeleteIcon/>
+                                    </IconButton>
+                                </div>
+                            )))
+                    }}/>
+                <Button
+                    className="custom-button"
+                    type="submit"
+                    variant="contained"
+                    color="primary">
+                    Submit
+                </Button>
             </div>
         )
     }
